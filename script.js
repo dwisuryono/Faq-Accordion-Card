bbc0b016a45926925686a813ada5274b9dad62b4
Dwi Suryono <dwisuryono209@gmail.com>
2021-12-03 16:53:12 +0700

    Updating File

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 const faqItems = document.querySelectorAll('.faq-items')
-// faqItems.forEach(function(el) {
-//     el.addEventListener('click', function() {
-//         const descFaqItems = document.querySelector('.desc-faq-items')
-//         descFaqItems.classList.toggle('.show')
-//     })
-// })
 
 for( let i = 0; i < faqItems.length; i++ ){
     faqItems[i].addEventListener('click', function() {
