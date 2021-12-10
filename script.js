9f799797fe4718904efa77ea41d9ac4985153a14
Dwi Suryono <dwisuryono209@gmail.com>
2021-12-10 09:54:05 +0700

    Update files

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const faqItems = document.querySelectorAll('.faq-items')
+const descFaqItems = document.querySelectorAll('.desc-faq-items')
 
 for( let i = 0; i < faqItems.length; i++ ){
-    faqItems[i].addEventListener('click', function() {
-        const descFaqItems = document.querySelectorAll('.desc-faq-items')
-        if( descFaqItems[i].style.display === 'block' ) {
-            descFaqItems[i].style.display = 'none'
-        } else {
-            descFaqItems[i].style.display = 'block'
-        }
-
-        const imgArrow =document.querySelectorAll('.img-arrow')
+    faqItems[i].addEventListener('click', function(e) {
+        e.target.nextElementSibling.classList.toggle('slide')
+        
+        const imgArrow = document.querySelectorAll('.img-arrow')
         imgArrow[i].classList.toggle('rotate')
-
         faqItems[i].classList.toggle('bold')
     })
 }
